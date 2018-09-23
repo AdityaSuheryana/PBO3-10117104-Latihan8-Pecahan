61de2c15b962aab52e34a49b7964f9774d16e9b0
Aditya Suheryana <adityasuheryanaa@gmail.com>
2018-09-23 18:25:05 +0700

    first commit

---
+++ src/pbo3/pkg10117104/latihan8/pecahan/PBO310117104Latihan8Pecahan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117104.latihan8.pecahan;
+
+/**
+ *
+ *@author  
+ * Nama      : Aditya Suheryana
+ * Kelas     : IF-3 / PBO-3
+ * Nim       : 10117104
+ * Deskripsi : Program ini berisi fungsi float dan double  
+ *
+ */
+public class PBO310117104Latihan8Pecahan {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
